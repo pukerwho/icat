4a142019a2b4a5977646a91318e8e5444c8068ef
Puker Who <nawwesele@gmail.com>
2023-11-16 11:32:24 +0200

    edit

--- inc/custom-fields/gutenberg-blocks/faq-block.php
+++ inc/custom-fields/gutenberg-blocks/faq-block.php
@@ -14,17 +14,16 @@ Block::make( 'TREBA FAQ Block' )
 		?>
     
     <div id="citylist-faq" class="mb-6">
-      <h2 class="mb-4">FAQ</h2>
       <div>
         <div itemscope itemtype="https://schema.org/FAQPage">
           <?php 
             $post_faqs = $fields['crb_block_faq']; 
             foreach( $post_faqs as $post_faq ):
           ?>
-            <div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" class="border-b border-gray-200 pb-4 mb-4 last-of-type:border-none last-of-type:pb-0 last-of-type:mb-0">
-              <div class="font-bold mb-2" itemprop="name"><?php echo $post_faq['crb_block_faq_question']; ?></div>
-              <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" class="font-light">
-                <div itemprop="text"><?php echo $post_faq['crb_block_faq_answer']; ?></div>
+            <div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" class="content border-b border-gray-200 pb-4 mb-4 last-of-type:border-none last-of-type:pb-0 last-of-type:mb-0">
+              <div class="text-lg font-bold mb-2" itemprop="name"><?php echo $post_faq['crb_block_faq_question']; ?></div>
+              <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                <p itemprop="text"><?php echo $post_faq['crb_block_faq_answer']; ?></p>
               </div>
             </div>
           <?php endforeach; ?>
