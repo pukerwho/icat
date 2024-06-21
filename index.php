d9a6e09b1bf2eebdc8c8a7877a439d9236243f79
Puker Who <nawwesele@gmail.com>
2024-06-21 14:59:32 +0300

    Edit

--- index.php
+++ index.php
@@ -77,9 +77,12 @@
       </div>
     <?php endwhile; endif; wp_reset_postdata(); ?>
   </div>
+
   <div class="mb-20">
     <div class="flex flex-wrap lg:-mx-6">
+      <!-- LEFT -->
       <div class="w-full lg:w-2/3 lg:px-6 mb-6 lg:mb-0">
+        <!-- POSTS -->
         <div class="flex justify-center">
           <h2 class="inline-block bg-theme-dark text-2xl text-gray-200 uppercase rounded-lg -rotate-2 px-4 py-2 mb-10"><?php _e("Новые публикации", "treba-wp"); ?></h2>
         </div> 
@@ -98,59 +101,11 @@
             </div>
           <?php endwhile; endif; wp_reset_postdata(); ?>
         </div>
-        <div class="text-center">
+        <div class="text-center mb-20">
           <a href="<?php echo get_page_url('page-blog'); ?>" class="bg-transparent hover:bg-theme-dark border-2 border-theme-dark text-theme-dark hover:text-gray-200 rounded-lg px-6 py-3"><?php _e("Все публикации", "treba-wp"); ?></a>
         </div>
-      </div>
-      <div class="w-full lg:w-1/3 lg:px-6">
-        <div class="sticky top-5">
-          <div class="bg-theme-dark text-gray-200 rounded-t-lg px-2 py-4">
-            <div class="text-xl text-center font-semibold"><?php _e("ТОП записи", "treba-wp"); ?></div>
-          </div>
-          <div class="bg-gray-100 rounded-b-lg px-4 py-4 mb-6">
-            <ul>
-              <li class="mb-3"><a href="https://icatalog.pro/kak-sdelat-gamburger-menyu-gotovyj-kod-i-podrobnoe-obyasnenie/" class="opacity-75 hover:text-blue-500 font-medium">Как сделать гамбургер меню</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/change-color-in-svg/" class="opacity-75 hover:text-blue-500 font-medium">Как изменить цвет SVG в CSS</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/poyavlenie-elementov-pri-skrolle/" class="opacity-75 hover:text-blue-500 font-medium">Появление элементов при скролле</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/kak-pozhalovatsya-na-telegram-kanal-podrobnaya-instrukciya/" class="opacity-75 hover:text-blue-500 font-medium">Как пожаловаться на Telegram канал</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/nick-v-telegram/" class="opacity-75 hover:text-blue-500 font-medium">Ник в Telegram</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/kak-vyrovnyat-kartinku-po-czentru-s-pomoshhyu-css/" class="opacity-75 hover:text-blue-500 font-medium">Как выровнять картинку по центру с помощью CSS</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/kak-sdelat-plavnoe-uvelichenie-kartinki-pri-navedenii-effekt-na-chistom-css/" class="opacity-75 hover:text-blue-500 font-medium">Как сделать плавное увеличение картинки при наведении</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/kak-sdelat-effekt-nazhatiya-knopki-na-css/" class="opacity-75 hover:text-blue-500 font-medium">Как сделать эффект нажатия кнопки на CSS</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/how-to-add-a-css-class-on-scrol/" class="opacity-75 hover:text-blue-500 font-medium"></a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/shcho-robyty-yakshcho-zablokuvaly-instahram/" class="opacity-75 hover:text-blue-500 font-medium">Заблокували Instagram: що робити</a></li>
-            </ul>
-          </div>
 
-          <div class="bg-theme-dark text-gray-200 rounded-t-lg px-2 py-4">
-            <div class="text-xl text-center font-semibold"><?php _e("Сейчас читают", "treba-wp"); ?></div>
-          </div>
-          <div class="bg-gray-100 rounded-b-lg px-4 py-4 mb-6">
-            <ul>
-              <li class="mb-3"><a href="https://icatalog.pro/7403-jak-pidpisati-podrugu-v-telefoni/" class="opacity-75 hover:text-blue-500 font-medium">Як підписати подругу в телефоні?</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/how-black-in-image/" class="opacity-75 hover:text-blue-500 font-medium">Как сделать затемнение фона (картинки) на CSS</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/kak-ochistit-kesh-v-telegram/" class="opacity-75 hover:text-blue-500 font-medium">Как очистить кэш в Telegram</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/kak-izmenit-czvet-knopki-polnaya-instrukcziya-dlya-novichkov/" class="opacity-75 hover:text-blue-500 font-medium">Как изменить цвет кнопки</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/najpopulyarnishi-polski-imena/" class="opacity-75 hover:text-blue-500 font-medium">Найпопулярніші польські імена</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/add-js-in-html/" class="opacity-75 hover:text-blue-500 font-medium">Правильно подключаем Javascript в HTML</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/samaya-vkusnaya-shaurma-v-kieve/" class="opacity-75 hover:text-blue-500 font-medium">Где в Киеве самая вкусная шаурма</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/seo-optimizacziya-sajta-na-tilde-polnoe-rukovodstvo/" class="opacity-75 hover:text-blue-500 font-medium">Seo-оптимизация сайта на Тильде</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/rokiv-vesillia-pryvitannia-vitannia-do-ahatovoho-vesillia/" class="opacity-75 hover:text-blue-500 font-medium">14 років весілля привітання</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/perenos-sajta-na-tilde-na-svoj-hosting-poshagovoe-rukovodstvo/" class="opacity-75 hover:text-blue-500 font-medium">Перенос сайта на Тильде на свой хостинг</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/kak-zagruzit-golosovoe-soobshchenie-v-telegram/" class="opacity-75 hover:text-blue-500 font-medium">Как скачать голосовое сообщение в Telegram</a></li>
-              <li class="mb-3"><a href="https://icatalog.pro/shho-prygotuvaty-yisty-z-nichogo-7-idej/" class="opacity-75 hover:text-blue-500 font-medium">Що приготувати їсти з нічого</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-  </div>
-
-  <!-- Объявления -->
-  <div class="mb-20">
-    <div class="flex flex-wrap lg:-mx-6">
-      <div class="w-full lg:w-2/3 lg:px-6">
+        <!-- ОГОЛОШЕННЯ -->
         <h2 class="inline-block bg-blue-500 text-2xl text-gray-200 uppercase rounded-lg -rotate-2 px-4 py-2 mb-10"><?php _e("ТОП объявления", "treba-wp"); ?></h2>
         <div class="mb-10">
           <?php 
@@ -170,12 +125,13 @@
           <a href="<?php echo get_page_url('page-items'); ?>" class="bg-transparent hover:bg-blue-500 border-2 border-blue-500 text-theme-dark hover:text-gray-200 rounded-lg px-6 py-3"><?php _e("Все объявления", "treba-wp"); ?></a>
         </div>
       </div>
+
+      <!-- RIGHT -->
       <div class="w-full lg:w-1/3 lg:px-6">
         <?php get_template_part('template-parts/sidebar'); ?>
       </div>
     </div>
   </div>
-  <!-- END Объявления -->
 </div>
 
 <?php get_footer(); ?>
--- template-parts/sidebar.php
+++ template-parts/sidebar.php
@@ -1,4 +1,62 @@
 <div class="bg-theme-dark text-gray-200 rounded-t-lg px-2 py-4">
+  <div class="text-xl text-center font-semibold"><?php _e("Популярные записи", "treba-wp"); ?></div>
+</div>
+<div class="bg-gray-100 rounded-b-lg px-4 py-4 mb-6">
+  <?php 
+  $new_posts = new WP_Query( array( 
+    'post_type' => 'post', 
+    'posts_per_page' => 10,
+    'orderby' => 'rand',
+    'tax_query' => array(
+      array(
+        'taxonomy'  => 'category',
+        'field'     => 'term_id',
+        'terms'     => array( 79, 4, 1, 82 ),
+      )
+    ),
+  ) );
+  if ($new_posts->have_posts()) : while ($new_posts->have_posts()) : $new_posts->the_post(); 
+  ?>
+  <div class="relative flex items-center border-b border-gray-300 pb-4 mb-4 last-of-type:pb-0 last-of-type:border-none">
+    <a href="<?php the_permalink(); ?>" class="w-full h-full absolute top-0 left-0 z-1"></a>
+    <div class="w-1/3"><img  src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="<?php the_title(); ?>" class="object-cover aspect-video"></div>
+    <div class="w-2/3"><div class="font-medium ml-4"><?php the_title(); ?></div></div>
+  </div>
+  <?php endwhile; endif; wp_reset_postdata(); ?>
+</div>
+
+<div class="bg-theme-dark text-gray-200 rounded-t-lg px-2 py-4">
+  <div class="text-xl text-center font-semibold"><?php _e("Сейчас читают", "treba-wp"); ?></div>
+</div>
+<div class="bg-gray-100 rounded-b-lg px-4 py-4 mb-6">
+  <?php 
+  $now_posts = new WP_Query( array( 
+    'post_type' => 'post', 
+    'posts_per_page' => 10,
+    'orderby' => 'rand',
+    'tax_query' => array(
+      array(
+        'taxonomy'  => 'category',
+        'field'     => 'term_id',
+        'terms'     => array( 79, 4, 1, 82 ),
+      )
+    ),
+  ) );
+  if ($now_posts->have_posts()) : while ($now_posts->have_posts()) : $now_posts->the_post(); 
+  ?>
+  <div class="relative border-b border-gray-300 pb-4 mb-4 last-of-type:pb-0 last-of-type:border-none">
+    <a href="<?php the_permalink(); ?>" class="w-full h-full absolute top-0 left-0 z-1"></a>
+    <div class="mb-4">
+      <img  src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="<?php the_title(); ?>" class="w-full h-full object-cover aspect-video">
+    </div>
+    <div class="font-medium text-center">
+      <?php the_title(); ?>
+    </div>
+  </div>
+  <?php endwhile; endif; wp_reset_postdata(); ?>
+</div>
+
+<div class="bg-theme-dark text-gray-200 rounded-t-lg px-2 py-4">
   <div class="text-xl text-center font-semibold"><?php _e("Категории", "treba-wp"); ?></div>
 </div>
 <div class="bg-gray-100 rounded-b-lg px-4 py-4 mb-6">
